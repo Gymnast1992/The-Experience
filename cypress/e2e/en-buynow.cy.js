556c549f82c1fe3229547cd35ccac19e990d44fe
Maksym Semiankiv <1992maksym1992@gmail.com>
2025-01-18 15:04:35 +0100

    buy now pom

--- cypress/e2e/en-buynow.cy.js
+++ cypress/e2e/en-buynow.cy.js
@@ -1,4 +1,5 @@
 import data from "../fixtures/data/data.json";
+import BuyNow from "../fixtures/pom/en.buynow.page";
 
 describe("Buy now", () => {
     beforeEach(() => {
@@ -6,8 +7,15 @@ describe("Buy now", () => {
     })
 
     it("Should navigate to the buy now section and verify three Amazon images", () => {
-        cy.get('#cardtitle1_776388310').should("have.text", data["Amazon.com"]);
-        cy.get('#cardtitle2_776388310').should("have.text", "\nAmazon.ca\n");
-        cy.get('#cardtitle3_776388310').should("have.text", "\nAmazon Kindle\n");
+        BuyNow.verifyTheTextAmazonCom(data["Amazon.com"]);
+        BuyNow.verifyTheTextAmazonCa(data["Amazon.ca"]);
+        BuyNow.verifyTheTextAmazonKindle(data.AmazonKindle);
+    })
+
+    it.only("The link Amazon.com sHould be clickable", () => {
+        cy.get('#cardtitle1_776388310').click();
+        cy.origin('https://www.amazon.com/dp/1779418388', () => {
+            cy.get('[id="title"]').should("be.visible");
+          })
     })
 })
--- cypress/fixtures/pom/en.buynow.page.js
+++ cypress/fixtures/pom/en.buynow.page.js
@@ -9,5 +9,17 @@ class BuyNow {
         return cy.get('#cardtitle3_776388310');
     }
 
-    
+    verifyTheTextAmazonCa(text) {
+        this.liAmazonCa.should("have.text", text);
+    }
+
+    verifyTheTextAmazonCom(text) {
+        this.liAmazonCom.should("have.text", text);
+    }
+
+    verifyTheTextAmazonKindle(text) {
+        this.liAmazonKindle.should("have.text", text);
+    }
 }
+
+export default new BuyNow();
