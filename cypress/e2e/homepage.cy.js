701ad1c69605a128b6ee83cecfcea2b8960d20d3
Maksym Semiankiv <1992maksym1992@gmail.com>
2026-03-07 16:41:23 +0100

    homepage 2nd test

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,5 +1,5 @@
 
-describe("Buy now", () => {
+describe("Homepage", () => {
   beforeEach(() => {
     cy.visit("/");
   });
@@ -7,6 +7,12 @@ describe("Buy now", () => {
   it("User should be able to switch language from FR to EN.", () => {
     cy.contains("EN").click({force: true});
     cy.contains("The Experience").should("be.visible");
+  });
+
+  it("User should be able to switch language from EN to FR.", () => {
+    cy.contains("EN").click({force: true});
+    cy.contains("FR").click({force: true});
+    cy.contains("L'expérience").should("be.visible");
   });
 
 });
