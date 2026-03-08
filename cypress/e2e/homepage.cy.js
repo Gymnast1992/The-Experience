bc8feb97fc96c22de98ebe6d25d3d030500c4d67
Maksym Semiankiv <1992maksym1992@gmail.com>
2026-03-08 18:04:50 +0100

    header verification test

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -15,4 +15,18 @@ describe("Homepage", () => {
     cy.contains("L'expérience").should("be.visible");
   });
 
+  it("The header should verify three buttons in the header ('About the book', 'Blog', 'Purchase')", () => {
+    cy.get('a [type="button"]').click({force: true});
+    cy.get('li.t199__menu-item-wrap').should("have.length", 3);
+    cy.get('li.t199__menu-item-wrap').eq(0).should("have.text", " À propos du livre ");
+    cy.get('li.t199__menu-item-wrap').eq(1).should("have.text", " Achat ");
+    cy.get('li.t199__menu-item-wrap').eq(2).should("have.text", " Blog ");
+  })
+
+  it("User should verify buttons EN and FR in the header", () => {
+    cy.get('a [type="button"]').click({force: true});
+    cy.get('[class="t199__lang-item"]').should("have.length", 2);
+    cy.get('[class="t199__lang-item"]').eq(0).should("have.text", "EN");
+    cy.get('[class="t199__lang-item"]').eq(1).should("have.text", "FR");
+  })
 });
