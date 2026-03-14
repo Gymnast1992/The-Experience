d8bb4cc7642f0f6e84df415b1502d66b60d1883e
Maksym Semiankiv <1992maksym1992@gmail.com>
2026-03-14 16:12:14 +0100

    code cleaning

--- cypress/e2e/en_homepage.cy.js
+++ cypress/e2e/en_homepage.cy.js
@@ -9,12 +9,18 @@ describe('Homepage_EN', () => {
     cy.get('h1.t-title').should('have.text', 'The Experience');
   });
 
-  it('TC_02, Verify user can switch language from English to French', () => {
+  it('TC_02, Verify EN and FR language buttons are visible in the header', () => {
+    cy.get('a [type="button"]').click({ force: true });
+    cy.get('[class="t199__lang-item"]').eq(0).should('have.text', 'EN');
+    cy.get('[class="t199__lang-item"]').eq(1).should('have.text', 'FR');
+  });
+
+  it('TC_03, Verify user can switch language from English to French', () => {
     cy.contains('FR').click({ force: true });
     cy.get('h1.t-title').should('have.text', "l'Expérience");
   });
 
-  it('TC_03, Verify 3 header buttons are displayed in the hamburger menu', () => {
+  it('TC_04, Verify 3 header buttons are displayed in the hamburger menu', () => {
     cy.get('a [type="button"]').click({ force: true });
     cy.get('li.t199__menu-item-wrap').should('have.length', 3);
     cy.get('li.t199__menu-item-wrap')
@@ -22,13 +28,6 @@ describe('Homepage_EN', () => {
       .should('have.text', ' About the book ');
     cy.get('li.t199__menu-item-wrap').eq(1).should('have.text', ' Purchase ');
     cy.get('li.t199__menu-item-wrap').eq(2).should('have.text', ' Blog ');
-  });
-
-  it('TC_04, Verify EN and FR language buttons are visible in the header', () => {
-    cy.get('a [type="button"]').click({ force: true });
-    cy.get('[class="t199__lang-item"]').should('have.length', 2);
-    cy.get('[class="t199__lang-item"]').eq(0).should('have.text', 'EN');
-    cy.get('[class="t199__lang-item"]').eq(1).should('have.text', 'FR');
   });
 
   it('TC_05, Verify "About the Book" button is clickable and navigates to the correct section', () => {
