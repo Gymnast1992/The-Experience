1dae512d3a462daee30290ed1d0a468f92fc4d56
Maksym Semiankiv <1992maksym1992@gmail.com>
2026-03-15 13:23:19 +0100

    homepage tests in french

--- cypress/e2e/french/homepage.cy.js
+++ cypress/e2e/french/homepage.cy.js
@@ -2,11 +2,11 @@ const { before } = require('lodash');
 
 describe('Homepage', () => {
   beforeEach(() => {
-    cy.visit('/en');
+    cy.visit('/');
   });
 
   it('TC_01, Verify homepage page title is displayed correctly', () => {
-    cy.get('h1.t-title').should('have.text', 'The Experience');
+    cy.get('h1.t-title').should('have.text', "l'Expérience");
   });
 
   it('TC_02, Verify EN and FR language buttons are visible in the header', () => {
@@ -15,9 +15,9 @@ describe('Homepage', () => {
     cy.get('[class="t199__lang-item"]').eq(1).should('have.text', 'FR');
   });
 
-  it('TC_03, Verify user can switch language from English to French', () => {
-    cy.contains('FR').click({ force: true });
-    cy.get('h1.t-title').should('have.text', "l'Expérience");
+  it('TC_03, Verify user can switch language from French to English', () => {
+    cy.contains('EN').click({ force: true });
+    cy.get('h1.t-title').should('have.text', 'The Experience');
   });
 
   it('TC_04, Verify 3 header buttons are displayed in the hamburger menu', () => {
@@ -25,20 +25,20 @@ describe('Homepage', () => {
     cy.get('li.t199__menu-item-wrap').should('have.length', 3);
     cy.get('li.t199__menu-item-wrap')
       .eq(0)
-      .should('have.text', ' About the book ');
-    cy.get('li.t199__menu-item-wrap').eq(1).should('have.text', ' Purchase ');
+      .should('have.text', ' À propos du livre ');
+    cy.get('li.t199__menu-item-wrap').eq(1).should('have.text', ' Achat ');
     cy.get('li.t199__menu-item-wrap').eq(2).should('have.text', ' Blog ');
   });
 
-  it('TC_05, Verify "About the Book" button is clickable and navigates to the correct section', () => {
+  it('TC_05, Verify "À propos du livre" button is clickable and navigates to the correct section', () => {
     cy.get('a [type="button"]').click({ force: true });
-    cy.contains('About the book').click();
+    cy.contains('À propos du livre').click();
     cy.get('.t814__btn').eq(1).should('be.visible');
   });
 
-  it('TC_06, Verify "Purchase" button is clickable and navigates to the correct section', () => {
+  it('TC_06, Verify "Achat" button is clickable and navigates to the correct section', () => {
     cy.get('a [type="button"]').click({ force: true });
-    cy.contains('Purchase').click();
+    cy.contains('Achat').click();
     cy.get('.t-card__col').should('be.visible');
   });
 
@@ -48,12 +48,12 @@ describe('Homepage', () => {
     cy.get('h1[field="title"]').should('be.visible');
   });
 
-  it('TC_08, Verify "Read my blog" button is clickable and navigates to the correct section', () => {
+  it('TC_08, Verify "En savoir plus" button is clickable and navigates to the correct section', () => {
     cy.get('a.t-btn').eq(0).click();
     cy.contains('Maksym Semiankiv').should('be.visible');
   });
 
-  it('TC_09, Verify #lifestyle section on the page', () => {
-    cy.contains('#lifestyle').should('be.visible');
+  it('TC_09, Verify #Styledevie section on the page', () => {
+    cy.contains('#Styledevie').should('be.visible');
   });
 });
