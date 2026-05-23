f5c15d0532006cdda86483fbaa8369d55affa8a0
Maksym Semiankiv <1992maksym1992@gmail.com>
2026-05-23 08:42:29 +0200

    FR added return home test cases from every page

--- cypress/e2e/french/magieduchapiteau.cy.js
+++ cypress/e2e/french/magieduchapiteau.cy.js
@@ -1,5 +1,7 @@
-import magieduchapiteauPage from "../../fixtures/pom/fr/magieduchapiteau.page";
+import magieduchapiteauPage from '../../fixtures/pom/fr/magieduchapiteau.page';
+import homepage from '../../fixtures/pom/fr/homepage';
 const mainTitleBlogTextFr = 'La Magie du Chapiteau';
+const mainTitleHomepageFr = "l'Expérience";
 
 describe('MagieduChapiteau', () => {
   beforeEach(() => {
@@ -8,9 +10,14 @@ describe('MagieduChapiteau', () => {
 
   it('TC_01, Verify the main title on the page', () => {
     magieduchapiteauPage.verifyMainTitleText(mainTitleBlogTextFr);
-  })
+  });
 
   it('TC_02, Verify the length of pictures listing', () => {
     magieduchapiteauPage.verifyLengthOfTheLi(13);
+  });
+
+  it('TC_03, Verify " Retour à laccueil " button navigates to the homepage', () => {
+    magieduchapiteauPage.clickReturnHomeBtn();
+    homepage.verifyMainTitleTextFr(mainTitleHomepageFr);
   });
 });
